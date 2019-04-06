c79cf7cac4350cbfaf5539a05449832bcfaf4778
jaibles <justciaranhanrahan@gmail.com>
2019-04-06 15:41:14 +0100

    init commit

---
+++ build/precache-manifest.abbd5b53f699105c5c9a2f8370fa0ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f0274e25717d1cd18d21",
+    "url": "/static/js/main.9034a266.chunk.js"
+  },
+  {
+    "revision": "3aac7cd3275df0e3513b",
+    "url": "/static/js/2.acf8a8e0.chunk.js"
+  },
+  {
+    "revision": "f0274e25717d1cd18d21",
+    "url": "/static/css/main.cd7a059b.chunk.css"
+  },
+  {
+    "revision": "8b5ca8bcf91d36960ef52710d84929ce",
+    "url": "/index.html"
+  }
+];
